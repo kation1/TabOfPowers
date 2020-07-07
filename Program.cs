f4a01567df756fe102604f77bdad1f8592786e1f
Kathryn Pidek <KathrynPidek@quickenloans.com>
2020-07-07 15:34:06 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace TabOfPowers
+{
+    class Program
+    {
+        public static void CreateTable()
+        {
+            Console.WriteLine("Enter an integer");
+            string number = Console.ReadLine();
+            //Display table of squares & cubes until integer
+
+            int length = int.Parse(number);
+            Console.WriteLine("Number\t\tSquared\t\tCubed");
+            Console.WriteLine("=======\t\t=======\t\t=======");
+
+            for (int i = 1; i <= length; i++)
+            {
+                Console.WriteLine($"{i}\t\t{(i * i)}\t\t{(i * i * i)}");
+            }
+        }
+
+        public static void ProgramStart()
+        {
+            //Prompt user for integer
+            Console.WriteLine("Do you want to learn your squares and cubes!?(Y/N)");
+
+            string userCont = Console.ReadLine();
+            while (userCont == "Y")
+            {
+                CreateTable();
+                //Prompts user to continue
+                Console.WriteLine("Do you want to go again? (Y/N)");
+                userCont = Console.ReadLine();
+            }
+            Console.WriteLine("Thank you for learning!");
+        }
+        static void Main(string[] args)
+        {
+            ProgramStart();
+        }
+    }
+}
